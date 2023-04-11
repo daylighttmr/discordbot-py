3f75618b77fa8b2e64f32f6d12c312fc253ade2a
daylighttmr <130370929+daylighttmr@users.noreply.github.com>
2023-04-11 13:03:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -40,6 +40,40 @@ async def rolldice(ctx):
     dice2 = random.randint(1, 6)
     dice_sum = dice1 + dice2
     await ctx.send(f"You rolled {dice1} and {dice2}. The total is {dice_sum}!")
+    
+    paragraphs = [
+    "This is the first paragraph.",
+    "Here is another paragraph.",
+    "This is the third paragraph.",
+    "Yet another paragraph.",
+    "And finally, a fifth paragraph."
+]
+
+# Create a Discord client
+client = discord.Client()
+
+# Event listener for when the bot is ready
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}.')
+
+# Event listener for when a message is received
+@client.event
+async def on_message(message):
+    # Ignore messages sent by the bot itself
+    if message.author == client.user:
+        return
+
+    # Check if the message starts with the command prefix and the command is "go"
+    if message.content.startswith('!go'):
+        # Choose a random paragraph from the list
+        random_paragraph = random.choice(paragraphs)
+        
+        # Create an embed with the paragraph as the description and "Random Paragraph" as the title
+        embed = discord.Embed(title="Random Paragraph", description=random_paragraph)
+        
+        # Send the embed to the same channel where the message was received
+        await message.channel.send(embed=embed)
 
 # Run the bot
 bot.run(TOKEN)
