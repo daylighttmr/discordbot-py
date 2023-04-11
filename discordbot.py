1471682a70fdf0d3a277143e17607a0b1ceb3348
daylighttmr <130370929+daylighttmr@users.noreply.github.com>
2023-04-11 12:32:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,23 +1,30 @@
 import discord
-from dotenv import load_dotenv
-import os
-import random
 from discord.ext import commands
+import re
+import random
 
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}.')
 
+@bot.command(name='sum')
+async def sum_numbers(ctx):
+    # Find all numbers in the message and sum them
+    message = ctx.message.content
+    numbers = re.findall(r'\d+', message)
+    sum_of_numbers = sum(map(int, numbers))
+    await ctx.send(f'The sum of the numbers in your message is {sum_of_numbers}!')
+
 @bot.command(name='roll')
-async def roll(ctx):
+async def roll_dice(ctx):
+    # Roll 2d6 dice
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     dice_sum = dice1 + dice2
     await ctx.send(f"You rolled {dice1} and {dice2}. The total is {dice_sum}!")
+
 
 @bot.event
 async def on_message(message):
