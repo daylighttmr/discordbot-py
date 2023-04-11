6e5d0650e9733d8ecf8a2efea4946a114aaf6108
daylighttmr <130370929+daylighttmr@users.noreply.github.com>
2023-04-11 13:50:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import random
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Load environment variables
 from dotenv import load_dotenv
@@ -10,6 +12,17 @@ load_dotenv()
 # Set command prefix and bot token from environment variables
 PREFIX = os.getenv('PREFIX')
 TOKEN = os.getenv('TOKEN')
+
+# Set up Google Sheets API credentials
+scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+creds = ServiceAccountCredentials.from_json_keyfile_name('daylighttmr-bcd50a44ed0c.json', scopes)
+client = gspread.authorize(creds)
+
+sheet_id = '17hI1pPPxGqAtPJuJzT9eP8ZbJeNE9eOmsFbLjk5Fo58'
+sheet = client.open_by_key(sheet_id).sheet1
+
+# Write to a cell in the Google Sheet
+sheet.update_cell(1, 1, 'Hello, world!')
 
 # Create bot instance
 bot = commands.Bot(command_prefix=PREFIX)
